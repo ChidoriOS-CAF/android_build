3c8dca90db440ffde6f94b7c6eb3f131bff0cf2f
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-07 08:22:55 -0700

    DO NOT MERGE: disable inclusion of chidori sepol
    Change-Id: Ia1bf98c2b2c5844207bf57f263d40c7bb767f014

--- core/config.mk
+++ core/config.mk
@@ -1097,7 +1097,7 @@ endif
 ifneq ($(CHIDORI_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/chidori/sepolicy/sepolicy.mk)
+#$(eval include vendor/chidori/sepolicy/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
